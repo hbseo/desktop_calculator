a164d35e656f7c71880c88dce186d0312e965c21
hellogaon <la_vega_@naver.com>
2016-12-01 12:48:54 +0900

    [20161201] Stack 코드 정리

--- Serious/Stack.java
+++ Serious/Stack.java
@@ -1,6 +1,6 @@
 interface Stack {
-     	      public boolean isEmpty();
-     	      public Object peek();
-     	      public void push(Object theObject);
-     	      public Object pop();
+    public boolean isEmpty();
+    public Object peek();
+    public void push(Object theObject);
+    public Object pop();
 }
